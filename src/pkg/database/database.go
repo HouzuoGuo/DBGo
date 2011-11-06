3b46cc0998b17c1deb45d1550eabddac2c6710bf
Houzuo Guo <guohouzuo@gmail.com>
2011-11-06 21:36:08 +1100

    Regulating error handling, will continue tomorrow

--- src/pkg/database/database.go
+++ src/pkg/database/database.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"table"
 	"util"
+	"st"
 )
 
 type Database struct {
@@ -11,13 +12,14 @@ type Database struct {
 	Tables map[string]*table.Table
 }
 
-func Open(path string) (db *Database) {
+func Open(path string) (db *Database, status int) {
 	db = new(Database)
 	var directory *os.File
 	var err os.Error
 	directory, err = os.Open(path)
 	if err != nil {
 		db = nil
+		status = st.CannotOpenDatabaseDirectory
 		return
 	}
 	defer directory.Close()
@@ -25,6 +27,7 @@ func Open(path string) (db *Database) {
 	fileInfo, err = directory.Readdir(0)
 	if err != nil {
 		db = nil
+		status = st.CannotReadDatabaseDirectory
 		return
 	}
 	for _, singleFileInfo := range fileInfo {
@@ -38,22 +41,26 @@ func Open(path string) (db *Database) {
 				case ".log":
 					_, exists := db.Tables[name]
 					if !exists {
-						db.Tables[name], err = table.Open(path, name)
-						if err != nil {
-							os.Exit(1)
+						var ret int
+						db.Tables[name], ret = table.Open(path, name)
+						if ret != st.OK {
+							db = nil
+							status = st.CannotOpenTableInDatabaseInit
+							return
 						}
 					}
 			}
 		}
 	}
 	db.Path = path
+	status = st.OK
 	return
 }
 
-func (db *Database) New (tableName string) (table *table.Table, ok bool) {
+func (db *Database) New (tableName string) (table *table.Table, status int) {
 	return
 }
 
-func (db *Database) Delete (tableName string) (table *table.Table, ok bool) {
+func (db *Database) Delete (tableName string) (table *table.Table, status int) {
 	return
 }
--- src/pkg/table/table.go
+++ src/pkg/table/table.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"column"
 	"constant"
+	"st"
 	"util"
 )
 
@@ -18,24 +19,26 @@ type Table struct {
 	ColumnsInOrder []*column.Column
 }
 
-func Open(path, name string) (table *Table, err os.Error) {
+func Open(path, name string) (table *Table, status int) {
 	table.Path = path
 	table.Name = name
-	err = table.Init()
+	status = table.Init()
 	return
 }
 
-func (table *Table) Init() (err os.Error) {
+func (table *Table) Init() (status int) {
 	table.RowLength = 0
 	table.NumberOfColumns = 0
 	table.Columns = make(map[string]*column.Column)
 	table.ColumnsInOrder = make([]*column.Column, 0)
-	err = table.OpenFiles()
-	if err != nil {
+	ret := table.OpenFiles()
+	if ret != st.OK {
+		status = st.CannotOpenTableFiles
 		return
 	}
 	defFileInfo, err := table.DefFile.Stat()
 	if err != nil {
+		status = st.CannotStatTableDefFile
 		return
 	}
 	content := make([]byte, defFileInfo.Size)
@@ -43,35 +46,51 @@ func (table *Table) Init() (err os.Error) {
 	table.NumberOfColumns = len(lines)
 	for _, line := range lines {
 		lengthName := strings.Split(line, ":")
-		var length int
-		length, err = strconv.Atoi(lengthName[0])
+		length, err := strconv.Atoi(lengthName[0])
+		if err != nil {
+			status = st.InvalidColumnDefinition
+			return
+		}
 		aColumn := &column.Column{Offset:table.RowLength, Length:length, Name:lengthName[1]}
 		table.Columns[lengthName[1]] = aColumn
 		table.ColumnsInOrder = append(table.ColumnsInOrder[:], aColumn)
 		table.RowLength += length
 	}
 	table.RowLength++
+	status = st.OK
 	return
 }
 
-func (table *Table) OpenFiles() (err os.Error) {
+func (table *Table) OpenFiles() (status int) {
+	var err os.Error
 	table.DefFile, err = os.OpenFile(table.DefFilePath, os.O_RDWR, constant.DataFilePerm)
 	if err == nil {
 		table.DataFile, err = os.OpenFile(table.DataFilePath, os.O_RDWR, constant.DataFilePerm)
+		status = st.OK
+		if err != nil {
+			status = st.CannotOpenTableDataFile
+		}
+	} else {
+		status = st.CannotOpenTableDefFile
 	}
 	return
 }
 
-func (table *Table) Flush() (ok bool){
+func (table *Table) Flush() (status int){
 	err := table.DefFile.Sync()
 	if err == nil {
 		err = table.DataFile.Sync()
-		ok = true
+		status = st.OK
+		if err != nil {
+			status = st.CannotFlushTableDataFile
+		}
+	} else {
+		status = st.CannotFlushTableDefFile
 	}
 	return
 }
 
-func (table *Table) Seek(rowNumber int) (ok bool) {
+func (table *Table) Seek(rowNumber int) (status int) {
 	numberOfRows, ok := table.NumberOfRows()
 	if ok && rowNumber < numberOfRows {
 		table.DataFile.Seek(int64(rowNumber * table.RowLength), 0)
@@ -169,6 +188,10 @@ func (table *Table) Update(rowNumber int, row map[string]string) (ok bool) {
 		}
 		ok = true
 	}
+	return
+}
+
+func (table *Table) Add(name string, length int) (ok bool) {
 	return
 }
 
